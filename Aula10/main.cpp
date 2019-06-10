167f20739978a01228d33ea927a3040145c97379
Guima <48738309+Guima00@users.noreply.github.com>
2019-06-10 20:45:21 -0300

    Add files via upload

---
+++ Aula10/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "ArvBin.h"
+using namespace std;
+
+int main()
+{
+    ArvBin arv;
+    arv.montaArvore();
+    arv.preOrdem();
+    cout<<endl;
+    cout<<arv.contanos()<<endl;
+    return 0;
+}
